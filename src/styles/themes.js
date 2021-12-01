96d7c843aad4f341dbb92d9cfc35f000f47aafab
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-01 23:25:42 +0530

    mui theme integrated

---
+++ src/styles/themes.js
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material";
+import { COLORS } from "./colors";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: COLORS.HIGHLAND,
+    },
+    secondary: {
+      main: COLORS.HIGHLAND,
+    },
+    text: {
+      primary: COLORS.TUNA,
+      secondary: COLORS.SLATE_GRAY,
+      disabled: COLORS.CASPER,
+      hint: COLORS.CASPER,
+    },
+    background: {
+      default: COLORS.WHITE,
+      paper: COLORS.WHITE,
+    },
+    divider: COLORS.ATHENS_GRAY,
+  },
+  typography: {
+    fontFamily: "Inter",
+    h1: {
+      fontFamily: "Amiko",
+    },
+    h2: {
+      fontFamily: "Amiko",
+    },
+  },
+  spacing: 8,
+  shape: {
+    borderRadius: 8,
+  },
+});
