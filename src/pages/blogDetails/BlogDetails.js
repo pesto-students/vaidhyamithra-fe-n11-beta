2c57926ff1d5580e5eb794532a3d7d02482a80a6
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-10 19:17:35 +0530

    temp commit - typography enhancement

--- src/pages/blogDetails/BlogDetails.js
+++ src/pages/blogDetails/BlogDetails.js
@@ -4,17 +4,19 @@ import {
   Details,
   DetailsContainer,
   TagSection,
-  RelatedSection
+  RelatedSection,
 } from "./blogDetails.styled";
 import BloggerDetails from "../../components/molecules/bloggerDetails";
 import BlogTag from "../../components/atoms/blogTag";
 import RelatedBlogs from "../../components/organisms/relatedBlogs";
+import Typography from "../../components/atoms/typography";
+import { TEXT_TYPE } from "../../components/atoms/typography/typography.constants";
 
 const BlogDetails = () => {
   return (
     <Details>
       <DetailsContainer>
-        <img src={bpImg} width="720px" />
+        <img src={bpImg} width="720px" alt="blog-display" />
         <Paragraph fontWeight="700" mt mb fontSize="34px">
           We need to index the haptic GB card.
         </Paragraph>
@@ -23,10 +25,10 @@ const BlogDetails = () => {
           publishedDate="March 12, 2021"
           position="relative"
         />
-        <Paragraph fontWeight="700" mt mb fontSize="20px">
+        <Typography variant={TEXT_TYPE.H3}>
           We need to index the haptic GB card.
-        </Paragraph>
-        <Paragraph fontSize="16px" fontFamily="Inter" color="#718797">
+        </Typography>
+        <Typography variant={TEXT_TYPE.BODY1}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
@@ -35,12 +37,11 @@ const BlogDetails = () => {
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </Paragraph>
-        <Paragraph fontWeight="700" mt mb fontSize="20px">
+        </Typography>
+        <Typography variant={TEXT_TYPE.H3}>
           We need to index the haptic GB card.
-        </Paragraph>
-        <Paragraph fontSize="16px" fontFamily="Inter" color="#718797">
+        </Typography>
+        <Typography variant={TEXT_TYPE.BODY1}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
@@ -49,8 +50,7 @@ const BlogDetails = () => {
           remaining essentially unchanged. It was popularised in the 1960s with
           the release of Letraset sheets containing Lorem Ipsum passages, and
           more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </Paragraph>
+        </Typography>
         <TagSection>
           <BlogTag>CARDIAC ISSUES</BlogTag>
           <BlogTag>BLOOD</BlogTag>
