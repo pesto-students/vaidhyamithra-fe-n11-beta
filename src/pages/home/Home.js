f6f0873e8c76bb539491866f69533764f61daaae
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-10 17:20:50 +0530

    combined all organisms for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,32 @@
+import { Divider } from "@mui/material";
 import { useState } from "react";
+import RecommendedTopics from "../../components/organisms/recommendedTopics/RecommendedTopics";
 import { TabMenu, TabPanel } from "../../components/organisms/tabs";
 import { homeMenuItems } from "./home.constants";
+import { LeftSection, RightSection, HomepageContainer } from "./home.styled";
 
 const Home = () => {
   const [currentTab, setCurrentTab] = useState(homeMenuItems[0].value);
 
   return (
-    <>
-      <TabMenu
-        value={currentTab}
-        setValue={setCurrentTab}
-        menuItems={homeMenuItems}
-      />
-      {homeMenuItems.map(({ value, component }) => (
-        <TabPanel key={value} value={value} currentValue={currentTab}>
-          {component}
-        </TabPanel>
-      ))}
-    </>
+    <HomepageContainer>
+      <LeftSection>
+        <TabMenu
+          value={currentTab}
+          setValue={setCurrentTab}
+          menuItems={homeMenuItems}
+        />
+        {homeMenuItems.map(({ value, component }) => (
+          <TabPanel key={value} value={value} currentValue={currentTab}>
+            {component}
+          </TabPanel>
+        ))}
+      </LeftSection>
+      <Divider orientation="vertical" />
+      <RightSection>
+        <RecommendedTopics />
+      </RightSection>
+    </HomepageContainer>
   );
 };
 
