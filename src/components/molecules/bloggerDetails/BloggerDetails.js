05f0c68b1f20ef67336e56a79354fd37062a72f8
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-10 19:30:02 +0530

    blogger alt txt

--- src/components/molecules/bloggerDetails/BloggerDetails.js
+++ src/components/molecules/bloggerDetails/BloggerDetails.js
@@ -1,4 +1,4 @@
-import {AuthorDetails, AuthorDiv, Paragraph} from './bloggerDetails.styled';
+import { AuthorDetails, AuthorDiv, Paragraph } from "./bloggerDetails.styled";
 import manImg from "../../../images/man_img.png";
 
 const BloggerDetails = ({ authorName, publishedDate }) => {
@@ -9,6 +9,7 @@ const BloggerDetails = ({ authorName, publishedDate }) => {
         width="46px"
         height="46px"
         style={{ borderRadius: "50%", padding: "2px" }}
+        alt="avatar"
       />
       <AuthorDiv>
         <Paragraph fontSize="16px" fontWeight="700" fontFamily="Inter">
