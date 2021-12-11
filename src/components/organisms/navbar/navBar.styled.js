15debc81f6b18a7642bc7ab61fe457bf29217e25
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-12 02:41:16 +0530

    breaking changes - app structure changed - moved from index.js to app.js

--- src/components/organisms/navbar/navBar.styled.js
+++ src/components/organisms/navbar/navBar.styled.js
@@ -1,27 +1,34 @@
 import styled from "styled-components";
 import { withTheme } from "@emotion/react";
+import { Link } from "react-router-dom";
 
-export const NavBarContainer = withTheme(styled("div") `
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    border-bottom: 1px solid #e1e2e2;
-    position: sticky;
-    top: 0px;
-    background: white;
-    z-index: 1000;
-    color: ${(props) => props.theme.palette.primary.main};
+export const NavBarContainer = withTheme(styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${(props) => props.theme.palette.divider};
+  position: sticky;
+  top: 0px;
+  background: ${(props) => props.theme.palette.background.default};
+  z-index: 1000;
 `);
 
-export const NavBarTitle = styled.div`
-    padding-left: 22px;
-    padding-top: 14px;
-    padding-bottom: 12px;
-`;
+export const NavBarTitle = withTheme(styled(Link)`
+  padding-left: 22px;
+  padding-top: 14px;
+  padding-bottom: 12px;
+  text-decoration: none;
+  color: ${(props) => props.theme.palette.primary.main};
+`);
 
-export const NavBarIconContainer = styled.div`
-    padding-right: 22px;
-    gap: 16px;
-    display: flex;
-    align-items: center;
-`;
+export const NavBarIconContainer = withTheme(styled("div")`
+  padding-right: 22px;
+  gap: 16px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  svg:hover {
+    color: ${(props) => props.theme.palette.primary.main};
+  }
+`);
