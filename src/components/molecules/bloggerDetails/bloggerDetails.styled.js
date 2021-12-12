c0d6f57767d7914b019e49cd8e972b97993f2b03
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-12 11:21:20 +0530

    formatting

--- src/components/molecules/bloggerDetails/bloggerDetails.styled.js
+++ src/components/molecules/bloggerDetails/bloggerDetails.styled.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Paragraph = styled.p`
-  font-family: ${props => props.fontFamily || "Amiko"};
-  font-size: ${props => props.fontSize || "24px"};
+  font-family: ${(props) => props.fontFamily || "Amiko"};
+  font-size: ${(props) => props.fontSize || "24px"};
   line-height: 1.2;
-  font-weight: ${props => props.fontWeight || '400'};
-  margin-top: ${props => props.mt ? "10px": "0px"};
-  margin-bottom: ${props => props.mb ? "10px": "0px"};
-  color: ${props => props.color || "black"}
+  font-weight: ${(props) => props.fontWeight || "400"};
+  margin-top: ${(props) => (props.mt ? "10px" : "0px")};
+  margin-bottom: ${(props) => (props.mb ? "10px" : "0px")};
+  color: ${(props) => props.color || "black"};
 `;
 
 export const AuthorDiv = styled.div``;
@@ -16,7 +16,7 @@ export const AuthorDetails = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
-  position: ${props => props.position};
+  position: ${(props) => props.position};
   bottom: 0px;
 `;
 
