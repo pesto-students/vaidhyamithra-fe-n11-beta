dea390ba2da8b4c42472c6d8794d76893bc14c26
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-19 11:26:29 +0530

    suspense fallback and linear progress added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "./values/routes";
 import Login from "./components/organisms/login";
 import SignUP from "./components/organisms/signUp";
-import PageNotFound from "./pages/pageNotFound/PageNotFound";
-import Home from "./pages/home";
-import AppSkeleton from "./components/organisms/appSkeleton";
-import Search from "./pages/searchBlog";
-import EditBlog from "./pages/editBlog";
-import Profile from "./pages/profile";
+import LinearProgress from "./components/atoms/linearProgress";
+
+const PageNotFound = lazy(() => import("./pages/pageNotFound/PageNotFound"));
+const Home = lazy(() => import("./pages/home"));
+const AppSkeleton = lazy(() => import("./components/organisms/appSkeleton"));
+const Search = lazy(() => import("./pages/searchBlog"));
+const EditBlog = lazy(() => import("./pages/editBlog"));
+const Profile = lazy(() => import("./pages/profile"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path={ROUTES.HOME} element={<AppSkeleton />}>
-        <Route index element={<Home />} />
-        <Route path={ROUTES.LOGIN} element={<Login />} />
-        <Route path={ROUTES.SIGNUP} element={<SignUP />} />
-        <Route path={ROUTES.SEARCH} element={<Search />} />
-        <Route path={ROUTES.CREATE} element={<EditBlog />} />
-        <Route path={ROUTES.MY_PROFILE} element={<Profile />} />
-        <Route path="*" exact={true} element={<PageNotFound />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<LinearProgress />}>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<AppSkeleton />}>
+          <Route index element={<Home />} />
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+          <Route path={ROUTES.SIGNUP} element={<SignUP />} />
+          <Route path={ROUTES.SEARCH} element={<Search />} />
+          <Route path={ROUTES.CREATE} element={<EditBlog />} />
+          <Route path={ROUTES.MY_PROFILE} element={<Profile />} />
+          <Route path="*" exact={true} element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
