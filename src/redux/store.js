61e33a8828476fe21a428be5d8c33e13f8c85769
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-19 12:39:53 +0530

    changed order of text

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { ALERTS_SLICE } from "./features/alerts/alerts.config";
 import { USER_SLICE } from "./features/user/user.config";
+import { MODALS_SLICE } from "./features/modals/modals.congif";
 
 import userReducer from "./features/user/user.slice";
 import alertsReducer from "./features/alerts/alerts.slice";
-import { MODALS_SLICE } from "./features/modals/modals.congif";
 import modalsReducer from "./features/modals/modals.slice";
 
 export const store = configureStore({
