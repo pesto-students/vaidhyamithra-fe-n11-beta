bb83e43aa8339f66c54a35845f38f8753e4a68dc
Dharmit Dosani <dharmitdosani@gmail.com>
2021-12-27 21:16:34 +0530

    blogtag css

--- src/components/atoms/blogTags/blogTag.styled.js
+++ src/components/atoms/blogTags/blogTag.styled.js
@@ -5,7 +5,7 @@ export const Topic = styled.div`
   text-align: center;
   color: white;
   border-radius: 16px;
-  font-size: 10px;
+  font-size: 12px;
   letter-spacing: 1.2px;
   width: max-content;
   padding: 2px 8px;
